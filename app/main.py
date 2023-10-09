090f46441b15c46b29192f48a9310fbef6671529
ben-smyth <ben.df.smyth@gmail.com>
2023-10-09 21:19:36 +0100

    removing Pipfile.lock and adding to gitignore

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from input import CVParser
 
-
 def main():
     a = CVParser("../inputData/docx/coolfreecv_resume_en_06_n.docx")
     string = a.convertToString()
 
     print(string) 
-
 
 if __name__ == "__main__":
     main()
